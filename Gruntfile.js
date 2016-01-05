77aae14de0e05bc46d186ad142f1e6a6db1f2976
joss <josselin54.chevalay@gmail.com>
2016-01-05 10:02:34 +0100

    ça marche pour le gruntFile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,27 @@
-module.export = function(grunt){
-
+module.exports = function(grunt){    
+    // config
+    grunt.initConfig({
+        
+        // unit testing
+        mochaTest : {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt', // Optionally capture the reporter output to a file 
+                    quiet: false, // Optionally suppress output to standard out (defaults to false) 
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false) 
+                },
+                src: ['test/**/*.js']
+            }
+        }
+        
+    });
+    
+    
+    // Add the grunt-mocha-test tasks. 
+    grunt.loadNpmTasks('grunt-mocha-test');
+    
+    
+    // Add Task
+    grunt.registerTask('default', ['mochaTest']);
 };
